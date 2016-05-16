417fdba69159edfed78b4fd6537c09ff4b0473d7
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-16 15:37:30 -0300

    Mudei duas variáveis pra local, nada de super relevante.

--- app/src/main/java/br/edu/insper/puncaovenosa/CharacterSelectionActivity.java
+++ app/src/main/java/br/edu/insper/puncaovenosa/CharacterSelectionActivity.java
@@ -7,90 +7,88 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class CharacterSelectionActivity extends AppCompatActivity {
-    private ImageButton[] imageButtons;
-    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
 
-        imageButtons = new ImageButton[10];
+        ImageButton[] imageButtons = new ImageButton[10];
 
-        this.imageButtons[0] = (ImageButton) findViewById(R.id.image_btn_blonde);
-        this.imageButtons[0].setOnClickListener(new View.OnClickListener() {
+        imageButtons[0] = (ImageButton) findViewById(R.id.image_btn_blonde);
+        imageButtons[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 0));
             }
         });
 
-        this.imageButtons[1] = (ImageButton) findViewById(R.id.image_btn_brunette);
-        this.imageButtons[1].setOnClickListener(new View.OnClickListener() {
+        imageButtons[1] = (ImageButton) findViewById(R.id.image_btn_brunette);
+        imageButtons[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 1));
             }
         });
 
-        this.imageButtons[2] = (ImageButton) findViewById(R.id.image_btn_brunette1);
-        this.imageButtons[2].setOnClickListener(new View.OnClickListener() {
+        imageButtons[2] = (ImageButton) findViewById(R.id.image_btn_brunette1);
+        imageButtons[2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 2));
             }
         });
 
-        this.imageButtons[3] = (ImageButton) findViewById(R.id.image_btn_brunette2);
-        this.imageButtons[3].setOnClickListener(new View.OnClickListener() {
+        imageButtons[3] = (ImageButton) findViewById(R.id.image_btn_brunette2);
+        imageButtons[3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 3));
             }
         });
 
-        this.imageButtons[4] = (ImageButton) findViewById(R.id.image_btn_brunette3);
-        this.imageButtons[4].setOnClickListener(new View.OnClickListener() {
+        imageButtons[4] = (ImageButton) findViewById(R.id.image_btn_brunette3);
+        imageButtons[4].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 4));
             }
         });
 
-        this.imageButtons[5] = (ImageButton) findViewById(R.id.image_btn_blonde_boy);
-        this.imageButtons[5].setOnClickListener(new View.OnClickListener() {
+        imageButtons[5] = (ImageButton) findViewById(R.id.image_btn_blonde_boy);
+        imageButtons[5].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 5));
             }
         });
 
-        this.imageButtons[6] = (ImageButton) findViewById(R.id.image_btn_brunette_boy);
-        this.imageButtons[6].setOnClickListener(new View.OnClickListener() {
+        imageButtons[6] = (ImageButton) findViewById(R.id.image_btn_brunette_boy);
+        imageButtons[6].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 6));
             }
         });
 
-        this.imageButtons[7] = (ImageButton) findViewById(R.id.image_btn_brunette1_boy);
-        this.imageButtons[7].setOnClickListener(new View.OnClickListener() {
+        imageButtons[7] = (ImageButton) findViewById(R.id.image_btn_brunette1_boy);
+        imageButtons[7].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 7));
             }
         });
 
-        this.imageButtons[8] = (ImageButton) findViewById(R.id.image_btn_brunette2_boy);
-        this.imageButtons[8].setOnClickListener(new View.OnClickListener() {
+        imageButtons[8] = (ImageButton) findViewById(R.id.image_btn_brunette2_boy);
+        imageButtons[8].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 8));
             }
         });
 
-        this.imageButtons[9] = (ImageButton) findViewById(R.id.image_btn_brunette3_boy);
-        this.imageButtons[9].setOnClickListener(new View.OnClickListener() {
+        imageButtons[9] = (ImageButton) findViewById(R.id.image_btn_brunette3_boy);
+        imageButtons[9].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CharacterSelectionActivity.this, GameActivity.class).putExtra("i", 9));
