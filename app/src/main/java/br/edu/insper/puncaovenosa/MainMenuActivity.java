e5e98199047fd91b15c5743a5e4c0336b5653933
gabicfa <gabrielacfa@al.insper.edu.br>
2016-05-17 09:27:16 -0300

    Alinhei boneco na cama e fiz uma pagina inicial melhor

--- app/src/main/java/br/edu/insper/puncaovenosa/MainMenuActivity.java
+++ app/src/main/java/br/edu/insper/puncaovenosa/MainMenuActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        Button buttonPlay = (Button) findViewById(R.id.play_btn);
+        ImageButton buttonPlay = (ImageButton) findViewById(R.id.play_btn);
 
         assert buttonPlay != null;
         buttonPlay.setOnClickListener(new View.OnClickListener() {
